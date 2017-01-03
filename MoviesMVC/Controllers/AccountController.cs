0ab6faea6db3727c1117275d15bd6eb911ba1a43
zurawskj <josephzurawski@comcast.net>
2017-01-02 17:06:24 -0700

    Changed user db to a file.  Fixed account DI error

--- MoviesMVC/Controllers/AccountController.cs
+++ MoviesMVC/Controllers/AccountController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using MoviesMVC.Models;
+using Microsoft.Practices.Unity;
 
 namespace MoviesMVC.Controllers
 {
@@ -15,6 +16,7 @@ namespace MoviesMVC.Controllers
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
 
+        [InjectionConstructor()]
         public AccountController()
         {
         }
